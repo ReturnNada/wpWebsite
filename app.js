f401ac5395d62c4a49276b1bbe05e811da6b70be
ReturnNada <156492056+ReturnNada@users.noreply.github.com>
2024-04-09 09:38:51 -0400

    Add files via upload
    Added basic movement

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-function getDirection(event){
-    const key = event.keyCode;
+const player = document.getElementById("player");
+const myDiv = document.getElementById("myDiv");
+let posx = 25;
+let posy = 425;
+player.style.top = "425px";
+player.style.left = "25px";
 
-    const w = 87;
-    const a = 65;
-    const s = 83;
-    const d = 68;
+window.addEventListener("keydown", move);
+
+function move(event){
+    switch(event.key){
+        case "w":
+            posy -= 20;
+            player.style.top = posy + "px";
+            break;
+        case "a":
+            posx -= 20;
+            player.style.left = posx + "px";
+            break;
+        case "s":
+            posy += 20;
+            player.style.top = posy + "px";
+            break;
+        case "d":
+            posx += 20;
+            player.style.left = posx + "px";
+            break;
+        default:
+            break;
+    }
 }
